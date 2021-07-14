1f0ee00aab410c8ce452c3694b6607096c31dede
Juan Minango <jcarlosminango@gmail.com>
2021-07-14 14:48:06 -0300

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    //debugShowCheckedModeBanner: false,
-    title: "Frases do dia",
-    home: Container(
-     // padding: EdgeInsets.only(top: 40), //Espacamento interno
-      margin: EdgeInsets.only(top:40), //Espacamento externo
-      decoration: BoxDecoration(
-        border: Border.all(width: 3.0, color: Colors.white)
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Instagram"),
+        backgroundColor: Colors.green,
       ),
-      /*contain, cover, fill, fitlHeight, fitWidth, none, scaleDown*/
-        child: Image.asset(
-        "imagens/mesa.jpg",
-        fit: BoxFit.contain,
-      )
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("Contenido Principal"),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.lightGreen,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Text("Texto1"),
+              Text("Texto2"),
+            ],
+          ),
+        ),
+      ),
     ),
   ));
 }
