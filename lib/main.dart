44398cd2275ce1e64d7683d89dcf83fd14715a95
Juan Minango <jcarlosminango@gmail.com>
2021-07-14 14:13:40 -0300

    Alinhamentos

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,20 @@ void main() {
     //debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
-      //color: Colors.white,
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0), //Espacamento interno
-      //margin: EdgeInsets.fromLTRB(0, 0, 0, 0), //Espacamento externo
-      //margin: EdgeInsets.only(top: 30, left: 20),
-      margin: EdgeInsets.all(20),
+     // padding: EdgeInsets.only(top: 40), //Espacamento interno
+      margin: EdgeInsets.only(top:40), //Espacamento externo
       decoration: BoxDecoration(
         border: Border.all(width: 3.0, color: Colors.white)
       ),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("t1"),
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Text("t2"),
-          ),
-          Text("t3")
-          /*Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget commodo tellus.",
-            textAlign: TextAlign.justify,
-          )*/
+          Text("T1"),
+          Text("T2"),
+          Text("T3")
         ],
-      ),
+      )
     ),
   ));
 }
