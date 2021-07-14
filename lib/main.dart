bae043c9d7f8a6b94aca2f9288718af4fa037cd7
Juan Minango <jcarlosminango@gmail.com>
2021-07-14 15:11:33 -0300

    Stateless & Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,63 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+/*
+Stateless -> Widgets que nao podem ser alterados (constantes)
+Stateful -> Widgets que podem ser alterados (variaveis)
+ */
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: HomeStateful(),
+  ));
+}
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  var _texto = "Juan";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Instagram"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            RaisedButton(
+              onPressed: (){
+                setState(() { //Metodo fue alterado; Configura um novo estado
+                  _texto = "Curso Flutter";
+                });
+              },
+              color: Colors.amber,
+              child: Text("Clique Aqui"),
+            ),
+            Text("Nome: $_texto")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
         backgroundColor: Colors.green,
       ),
       body: Padding(
@@ -25,6 +76,8 @@ void main() {
           ),
         ),
       ),
-    ),
-  ));
+    );
+
+  }
 }
+
